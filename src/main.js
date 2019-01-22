383139e3ecd17b4be6932dfff70cc5eaaba06532
aldo1400 <achoque1400@gmail.com>
2019-01-22 17:11:34 -0500

    Uninstall fontawesome

--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee,faUser } from '@fortawesome/free-solid-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCoffee,faUser } from '@fortawesome/free-solid-svg-icons'
 import locale from 'element-ui/lib/locale/lang/en'
 // import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+// import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.component('font-awesome-layers', FontAwesomeLayers)
-Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Vue.component('font-awesome-layers', FontAwesomeLayers)
+// Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 
-library.add(faCoffee)
-library.add(faUser)
+// library.add(faCoffee)
+// library.add(faUser)
 
 // Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(ElementUI,{locale});
